6573e8f400244db39fcd4692989239bdeed7cd54
Fernando Miçalli //fermads <falves@uolinc.com>
2015-10-09 18:47:20 -0300

    error if settings file does not exist

--- AutoFold.py
+++ AutoFold.py
@@ -1,26 +1,25 @@
 import sublime, sublime_plugin, re
 
 class AutoFoldListener(sublime_plugin.EventListener):
-  s = False
+  settings = False
 
-  def loadSettings(self):
-    print("loading settings")
-    self.s = sublime.load_settings("AutoFold.sublime-settings")
+  def load_settings(self):
+    self.settings = sublime.load_settings("AutoFold.sublime-settings")
+    if not self.settings:
+      print "AutoFold.sublime-settings file is required"
 
   def on_pre_save_async(self, view):
-    if not self.s:
-      self.loadSettings()
+    if not self.settings:
+      self.load_settings()
 
-    attrs = self.s.get('foldAttributes')
-    tags = self.s.get('foldTags')
+    attrs = self.settings.get('attributes')
+    tags = self.settings.get('tags')
 
     if attrs:
       self.fold_attributes(view, attrs)
 
     if tags:
       self.fold_tags(view, tags)
-
-    print("OK, saved")
 
   def fold_tags(self, view, tags):
     for tag in tags:
@@ -33,19 +32,3 @@ class AutoFoldListener(sublime_plugin.EventListener):
       result = view.find_all(r"(?<=" + re.escape(attr)
              + "=\").*?(?=\")", sublime.IGNORECASE)
       view.fold(result)
-
-
-  # def on_post_save_async(self, view):
-  #   if not self.s:
-  #     self.loadSettings()
-  #   if self.s.get('attributes'):
-  #     print(self.s.get('attributes')[0])
-      # view.run_command('git_annotate')
-
-
-  # def run(self, edit):
-  #   # (https?|file|mailto)://(?:[^\s]+)+
-  #   result = self.view.find_all(r"(?<=\").*?(?=\")",sublime.IGNORECASE)
-  #   self.view.fold(result)
-  #   print(result)
-  #   # self.view.insert(edit, 0, "Hello, World!")
